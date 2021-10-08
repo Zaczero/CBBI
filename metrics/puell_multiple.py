0a7c98e2cd6cfff9a9efc6f8d2b8c5106bf6a430
Kamil Monicz <kamil@monicz.pl>
2021-10-08 21:59:50 +0200

    Fetch Puell from lookintobitcoin

--- metrics/puell_multiple.py
+++ metrics/puell_multiple.py
@@ -2,12 +2,46 @@ from typing import List
 
 import numpy as np
 import pandas as pd
+import requests
 import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-from utils import add_common_markers, mark_highs_lows
+from globals import HTTP_TIMEOUT
+from utils import add_common_markers
 from .base_metric import BaseMetric
+
+
+def _fetch_df() -> pd.DataFrame:
+    request_data = {
+        'output': 'chart.figure',
+        'changedPropIds': [
+            'url.pathname'
+        ],
+        'inputs': [
+            {
+                'id': 'url',
+                'property': 'pathname',
+                'value': '/charts/puell_multiple/'
+            }
+        ]
+    }
+
+    response = requests.post(
+        'https://www.lookintobitcoin.com/django_plotly_dash/app/puell_multiple/_dash-update-component', json=request_data,
+        timeout=HTTP_TIMEOUT)
+    response.raise_for_status()
+    response_json = response.json()
+    response_x = response_json['response']['props']['figure']['data'][1]['x']
+    response_y = response_json['response']['props']['figure']['data'][1]['y']
+
+    df = pd.DataFrame({
+        'Date': response_x[:len(response_y)],
+        'Puell': response_y,
+    })
+    df['Date'] = pd.to_datetime(df['Date']).dt.tz_localize(None)
+
+    return df
 
 
 class PuellMetric(BaseMetric):
@@ -20,15 +54,11 @@ class PuellMetric(BaseMetric):
         return 'Puell Multiple'
 
     def calculate(self, df: pd.DataFrame, ax: List[plt.Axes]) -> pd.Series:
-        df['PuellMA365'] = df['TotalGenerationUSD'].rolling(365).mean()
-        df['Puell'] = df['TotalGenerationUSD'] / df['PuellMA365']
+        df = df.merge(_fetch_df(), on='Date', how='left')
+        df['Puell'].ffill(inplace=True)
         df['PuellLog'] = np.log(df['Puell'])
 
-        df = mark_highs_lows(df, 'PuellLog', True, 120, 120)
-        df.fillna({'PuellLogHigh': 0, 'PuellLogLow': 0}, inplace=True)
-        df.loc[df['PuellLog'] < 1.1, 'PuellLogHigh'] = 0
-
-        high_rows = df.loc[df['PuellLogHigh'] == 1]
+        high_rows = df.loc[df['PriceHigh'] == 1]
         high_x = high_rows.index.values.reshape(-1, 1)
         high_y = high_rows['PuellLog'].values.reshape(-1, 1)
 
