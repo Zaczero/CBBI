21489f0b65e4e79b05543aed30acb81ab334799f
Kamil Monicz <kamil@monicz.pl>
2021-10-21 10:22:52 +0200

    Google Trends fetch in hourly interval for the past week

--- metrics/google_trends.py
+++ metrics/google_trends.py
@@ -9,16 +9,89 @@ from filecache import filecache
 from matplotlib import pyplot as plt
 from pytrends.request import TrendReq
 from sklearn.linear_model import LinearRegression
+from tqdm import tqdm
 
 from utils import mark_highs_lows, add_common_markers
 from .base_metric import BaseMetric
 
 
-@filecache(3600 * 24 * 3)  # 3 day cache
-def fetch_google_trends_data(keyword: str, timeframe: str) -> pd.DataFrame:
+@filecache(3600 * 24 * 3)  # cache for 3 days
+def _fetch_google_trends_data(keyword: str, timeframe: str) -> pd.DataFrame:
     pytrends = TrendReq(retries=5, backoff_factor=1)
     pytrends.build_payload(kw_list=[keyword], timeframe=timeframe)
-    return pytrends.interest_over_time()
+
+    df = pytrends.interest_over_time()
+    df.drop(columns=['isPartial'], inplace=True)
+    df.reset_index(inplace=True)
+    df.rename(columns={
+        'date': 'Date',
+        keyword: 'Interest'
+    }, inplace=True)
+
+    return df
+
+
+def _normalize(df: pd.DataFrame, df_fetch: pd.DataFrame) -> pd.DataFrame:
+    if df.shape[0] == 0:
+        return df_fetch
+
+    df_inner = df.merge(df_fetch, how='inner', on='Date')
+    overlap_days = df_inner.shape[0]
+
+    prev_scale = np.max(df_inner['Interest_x'])
+    next_scale = np.max(df_inner['Interest_y'])
+    ratio_scale = next_scale / prev_scale
+
+    if ratio_scale > 1:
+        df_fetch['Interest'] /= ratio_scale
+    elif ratio_scale < 1:
+        df['Interest'] *= ratio_scale
+
+    return df_fetch.iloc[overlap_days:]
+
+
+def _fetch_df(keyword: str, date_from: pd.Timestamp, date_to: pd.Timestamp) -> pd.DataFrame:
+    delta_days = 269  # 270 days will cause Google Trends API return weekly format
+    overlap_days = 200
+
+    df = pd.DataFrame()
+
+    date_to -= timedelta(5)  # last 7 days are set by _set_last_week_from_hourly
+    date_current = date_from
+    iter_count = int(np.ceil((date_to - date_from) / timedelta(delta_days - overlap_days)))
+
+    for _ in tqdm(range(iter_count), desc='Fetching daily Google Trends'):
+        timeframe_from = date_current.strftime('%Y-%m-%d')
+        date_current = min(date_current + timedelta(delta_days), date_to)
+        timeframe_to = date_current.strftime('%Y-%m-%d')
+        date_current -= timedelta(overlap_days - 1)
+
+        timeframe = f'{timeframe_from} {timeframe_to}'
+        df_fetch = _fetch_google_trends_data(keyword, timeframe)
+        df_fetch = _normalize(df, df_fetch)
+
+        df = df.append(df_fetch)
+
+    df = _set_last_week_from_hourly(df, keyword)
+
+    return df
+
+
+def _set_last_week_from_hourly(df: pd.DataFrame, keyword: str) -> pd.DataFrame:
+    df_fetch = _fetch_last_week(keyword)
+    df_fetch = df_fetch.resample('1D', on='Date').max()
+    df_fetch.drop(columns=['Date'], inplace=True)
+    df_fetch.reset_index(inplace=True)
+    df_fetch = _normalize(df, df_fetch)
+
+    return df.append(df_fetch)
+
+
+def _fetch_last_week(keyword: str) -> pd.DataFrame:
+    timeframe = f'now 7-d'
+    df_fetch = _fetch_google_trends_data(keyword, timeframe)
+
+    return df_fetch
 
 
 class GoogleTrendsMetric(BaseMetric):
@@ -31,77 +104,40 @@ class GoogleTrendsMetric(BaseMetric):
         return '"Bitcoin" search term (Google Trends)'
 
     def calculate(self, df: pd.DataFrame, ax: List[plt.Axes]) -> pd.Series:
+        keyword = 'Bitcoin'
+        days_shift = 1
         drop_off_per_day = 0.012
 
-        keyword = 'Bitcoin'
-        date_start = df.iloc[0]['Date'] - timedelta(90)
-        date_end = df.iloc[-1]['Date']
-        date_current = date_start
+        df_start_date = df.iloc[0]['Date']
+        date_from = df_start_date - timedelta(90)
+        date_to = df.iloc[-1]['Date']
 
-        delta_days = 269  # 270 days will cause Google Trends API return weekly format
-        match_window_days = 200  # int(np.floor(delta_days / 2)) + 1
-        iteration_count = int(np.ceil((date_end - date_start) / timedelta(delta_days - match_window_days)))
+        df = df.merge(_fetch_df(keyword, date_from, date_to), on='Date', how='right')
+        df['Interest'] = df['Interest'].shift(days_shift, fill_value=np.nan)
 
-        df_interest = pd.DataFrame()
-        cli_ui.info_1('Google Trends progress: [', end='')
+        df = mark_highs_lows(df, 'Interest', False, round(365 * 1.5), 365)
+        df.fillna({'InterestHigh': 0, 'InterestLow': 0}, inplace=True)
 
-        for i in range(iteration_count):
-            if i % 2 == 0:
-                print('#', end='')
+        for _, row in df.loc[df['InterestHigh'] == 1].iterrows():
+            from_idx = np.min(df.loc[(df.index > row.name) & (df['Interest'] < row['Interest'] / 3)].index)
+            df.loc[df.index >= from_idx, 'PreviousInterest'] = row['Interest']
 
-            date_start_str = date_current.strftime('%Y-%m-%d')
-            date_current = min(date_current + timedelta(delta_days), date_end)
-            date_end_str = date_current.strftime('%Y-%m-%d')
-            date_current -= timedelta(match_window_days - 1)
+        df['InterestScale'] = df['Interest'] / df['PreviousInterest']
 
-            timeframe = f'{date_start_str} {date_end_str}'
-            df_fetched = fetch_google_trends_data(keyword, timeframe)
-
-            if df_interest.shape[0] > 0:
-                prev_scale = np.max(df_interest.iloc[-match_window_days:][keyword])
-                next_scale = np.max(df_fetched.iloc[:match_window_days][keyword])
-                ratio_scale = next_scale / prev_scale
-
-                if ratio_scale > 1:
-                    df_fetched[keyword] /= ratio_scale
-                elif ratio_scale < 1:
-                    df_interest[keyword] *= ratio_scale
-
-                df_fetched = df_fetched.iloc[match_window_days:]
-
-            df_interest = df_interest.append(df_fetched)
-
-        print(']')
-
-        df_interest.reset_index(inplace=True)
-        df_interest.rename(columns={
-            'date': 'Date',
-            keyword: 'Interest'
-        }, inplace=True)
-        df_interest = mark_highs_lows(df_interest, 'Interest', False, round(365 * 1.5), 365)
-
-        for _, row in df_interest.loc[df_interest['InterestHigh'] == 1].iterrows():
-            from_idx = np.min(df_interest.loc[(df_interest.index > row.name) & (df_interest['Interest'] < row['Interest'] / 3)].index)
-            df_interest.loc[df_interest.index >= from_idx, 'PreviousInterestHigh'] = row['Interest']
-
-        df_interest['InterestScaleActual'] = df_interest['Interest'] / df_interest['PreviousInterestHigh']
-
-        high_rows = df_interest.loc[(df_interest['InterestHigh'] == 1) & ~ (df_interest['PreviousInterestHigh'].isna())]
+        high_rows = df.loc[(df['InterestHigh'] == 1) & ~ (df['PreviousInterest'].isna())]
         high_x = high_rows.index.values.reshape(-1, 1)
-        high_y = high_rows['InterestScaleActual'].values.reshape(-1, 1)
+        high_y = high_rows['InterestScale'].values.reshape(-1, 1)
 
-        x = df_interest.index.values.reshape(-1, 1)
+        x = df.index.values.reshape(-1, 1)
 
         lin_model = LinearRegression()
         lin_model.fit(high_x, high_y)
-        df_interest['InterestScaleModel'] = lin_model.predict(x)
+        df['InterestScaleModel'] = lin_model.predict(x)
 
-        df = df.merge(df_interest, on='Date', how='left')
-        df.fillna({'InterestHigh': 0, 'InterestLow': 0}, inplace=True)
-        df['Interest'].ffill(inplace=True)
-        df['PreviousInterestHigh'].ffill(inplace=True)
-        df['InterestScaleModel'].ffill(inplace=True)
-        df['GoogleTrends'] = df['Interest'] / (df['InterestScaleModel'] * df['PreviousInterestHigh'])
+        df = df.loc[df['Date'] >= df_start_date]
+        df.reset_index(inplace=True)
+
+        df['GoogleTrends'] = df['Interest'] / (df['InterestScaleModel'] * df['PreviousInterest'])
 
         def calculate_drop_off(rows_ref: np.ndarray):
             rows = np.copy(rows_ref)
@@ -125,8 +161,8 @@ class GoogleTrendsMetric(BaseMetric):
         add_common_markers(df, ax[0])
 
         sns.lineplot(data=df, x='Date', y='Interest', ax=ax[1])
-        sns.lineplot(data=df, x='Date', y='PreviousInterestHigh', ax=ax[1])
-        sns.lineplot(data=df, x='Date', y='InterestScaleActual', ax=ax[1])
+        sns.lineplot(data=df, x='Date', y='PreviousInterest', ax=ax[1])
+        sns.lineplot(data=df, x='Date', y='InterestScale', ax=ax[1])
         sns.lineplot(data=df, x='Date', y='InterestScaleModel', ax=ax[1])
         add_common_markers(df, ax[1], price_line=False)
 
