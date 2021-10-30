9e873ef0b808da1274067ca91e1af208f18a7434
Kamil Monicz <kamil@monicz.pl>
2021-10-31 00:57:00 +0200

    Added 2YMA lookintobitcoin fetch

--- metrics/two_year_moving_average.py
+++ metrics/two_year_moving_average.py
@@ -2,12 +2,49 @@ from typing import List
 
 import numpy as np
 import pandas as pd
+import requests
 import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
 
+from globals import HTTP_TIMEOUT
 from utils import add_common_markers
 from .base_metric import BaseMetric
+
+
+def _fetch_df() -> pd.DataFrame:
+    request_data = {
+        'output': 'chart.figure',
+        'changedPropIds': [
+            'url.pathname'
+        ],
+        'inputs': [
+            {
+                'id': 'url',
+                'property': 'pathname',
+                'value': '/charts/bitcoin-investor-tool/'
+            }
+        ]
+    }
+
+    response = requests.post(
+        'https://www.lookintobitcoin.com/django_plotly_dash/app/market_cycle_ma/_dash-update-component',
+        json=request_data,
+        timeout=HTTP_TIMEOUT
+    )
+
+    response.raise_for_status()
+    response_json = response.json()
+    response_x = response_json['response']['props']['figure']['data'][2]['x']
+    response_y = response_json['response']['props']['figure']['data'][2]['y']
+
+    df = pd.DataFrame({
+        'Date': response_x[:len(response_y)],
+        '2YMA': response_y,
+    })
+    df['Date'] = pd.to_datetime(df['Date']).dt.tz_localize(None)
+
+    return df
 
 
 class TwoYearMovingAverageMetric(BaseMetric):
@@ -20,7 +57,8 @@ class TwoYearMovingAverageMetric(BaseMetric):
         return '2 Year Moving Average'
 
     def calculate(self, df: pd.DataFrame, ax: List[plt.Axes]) -> pd.Series:
-        df['2YMA'] = df['Price'].rolling(365 * 2).mean()
+        df = df.merge(_fetch_df(), on='Date', how='left')
+        df['2YMA'].ffill(inplace=True)
         df['2YMALog'] = np.log(df['2YMA'])
         df['2YMAx5'] = df['2YMA'] * 5
         df['2YMAx5Log'] = np.log(df['2YMAx5'])
@@ -29,11 +67,11 @@ class TwoYearMovingAverageMetric(BaseMetric):
         df['2YMALogOvershootActual'] = df['PriceLog'] - df['2YMAx5Log']
         df['2YMALogUndershootActual'] = df['PriceLog'] - df['2YMALog']
 
-        high_rows = df.loc[(df['PriceHigh'] == 1) & ~ (df['2YMA'].isna())]
+        high_rows = df.loc[df['PriceHigh'] == 1]
         high_x = high_rows.index.values.reshape(-1, 1)
         high_y = high_rows['2YMALogOvershootActual'].values.reshape(-1, 1)
 
-        low_rows = df.loc[(df['PriceLow'] == 1) & ~ (df['2YMA'].isna())]
+        low_rows = df.loc[df['PriceLow'] == 1]
         low_x = low_rows.index.values.reshape(-1, 1)
         low_y = low_rows['2YMALogUndershootActual'].values.reshape(-1, 1)
 
