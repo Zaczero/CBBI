734a39e3c9b9236ee24fe3b51d304a29c60ae9dc
Kamil Monicz <kamil@monicz.pl>
2021-11-01 16:38:57 +0100

    Adjusted MVRV low model

--- metrics/mvrv_z_score.py
+++ metrics/mvrv_z_score.py
@@ -55,11 +55,12 @@ class MVRVMetric(BaseMetric):
 
     def calculate(self, df: pd.DataFrame, ax: List[plt.Axes]) -> pd.Series:
         bull_days_shift = 6
+        low_model_adjust = 0.26
 
         df = df.merge(_fetch_df(), on='Date', how='left')
         df.loc[df['DaysSinceHalving'] < df['DaysSincePriceLow'], 'MVRV'] = df['MVRV'].shift(bull_days_shift)
         df['MVRV'].ffill(inplace=True)
-        df['MVRV'] = np.log(1 + df['MVRV'])
+        df['MVRV'] = np.log(df['MVRV'] + 1)
 
         df = mark_highs_lows(df, 'MVRV', True, round(365 * 2), 365)
         df.fillna({'MVRVHigh': 0, 'MVRVLow': 0}, inplace=True)
@@ -76,22 +77,22 @@ class MVRVMetric(BaseMetric):
 
         lin_model = LinearRegression()
         lin_model.fit(high_x, high_y)
-        df['MVRVHighModel'] = lin_model.predict(x)
+        df['HighModel'] = lin_model.predict(x)
 
         lin_model.fit(low_x, low_y)
-        df['MVRVLowModel'] = lin_model.predict(x)
+        df['LowModel'] = lin_model.predict(x) + low_model_adjust
 
-        df['MVRVIndex'] = (df['MVRV'] - df['MVRVLowModel']) / \
-                          (df['MVRVHighModel'] - df['MVRVLowModel'])
+        df['Index'] = (df['MVRV'] - df['LowModel']) / \
+                      (df['HighModel'] - df['LowModel'])
 
-        df['MVRVIndexNoNa'] = df['MVRVIndex'].fillna(0)
+        df['IndexNoNa'] = df['Index'].fillna(0)
         ax[0].set_title(self.description)
-        sns.lineplot(data=df, x='Date', y='MVRVIndexNoNa', ax=ax[0])
+        sns.lineplot(data=df, x='Date', y='IndexNoNa', ax=ax[0])
         add_common_markers(df, ax[0])
 
         sns.lineplot(data=df, x='Date', y='MVRV', ax=ax[1])
-        sns.lineplot(data=df, x='Date', y='MVRVHighModel', ax=ax[1])
-        sns.lineplot(data=df, x='Date', y='MVRVLowModel', ax=ax[1])
+        sns.lineplot(data=df, x='Date', y='HighModel', ax=ax[1])
+        sns.lineplot(data=df, x='Date', y='LowModel', ax=ax[1])
         add_common_markers(df, ax[1], price_line=False)
 
-        return df['MVRVIndex']
+        return df['Index']
