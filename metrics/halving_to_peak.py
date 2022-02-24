3340c376633e4e7d72bdf4a187bf376658db1da9
Kamil Monicz <kamil@monicz.pl>
2022-02-24 21:31:00 +0100

    Added force HalvingToPeak decline for smooth metric removal transition

--- metrics/halving_to_peak.py
+++ metrics/halving_to_peak.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -44,6 +46,12 @@ class HalvingToPeakMetric(BaseMetric):
         df.loc[(peak_decline_after_days < df['DaysSincePriceHigh']) & (df['DaysSincePriceHigh'] < df['DaysSincePriceLow']), 'HalvingToPeakIndex'] = 1 - (df['DaysSincePriceHigh'] - peak_decline_after_days) / peak_decline_duration
         df.loc[(peak_decline_after_days + peak_decline_duration < df['DaysSincePriceHigh']) & (df['DaysSincePriceHigh'] < df['DaysSincePriceLow']), 'HalvingToPeakIndex'] = 0
         df['HalvingToPeakIndex'] = df['HalvingToPeakIndex'].fillna(0)
+
+        # force decline until the metric averages out with the CBBI score to be later removed
+        force_decline_start = pd.to_datetime('2022-02-23')
+        force_decline_duration = timedelta(days=12)
+        df.loc[force_decline_start <= df['Date'], 'HalvingToPeakIndex'] = \
+            df['HalvingToPeakIndex'] * np.maximum(1 - (df['Date'] - force_decline_start) / force_decline_duration, 0)
 
         ax[0].set_title(self.description)
         sns.lineplot(data=df, x='Date', y='HalvingToPeakIndex', ax=ax[0])
